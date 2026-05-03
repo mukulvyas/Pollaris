244dd4c290efca0886751b09f0da87b3de123751
mukulvyas <mukulvyas19@gmail.com>
2026-05-03 22:05:58 +0530

    fix: add service worker kill switch for normal browser support

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,7 +1,17 @@
-import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+
+// Force-clear any old buggy Service Workers
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.getRegistrations().then(registrations => {
+    for (let registration of registrations) {
+      registration.unregister();
+      console.log('Old Service Worker cleared');
+    }
+  });
+}
+
 import './utils/firebase';
 
 import ErrorBoundary from './components/ErrorBoundary'
